f7b54c8165b7a2d9de5eaaaedf9118ea3f81fb20
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-11-30 13:13:54 +1000

    Update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const ERC20Token = artifacts.require('ERC20Token');
+const ContractRegistry = artifacts.require('ContractRegistry');
+const QuestionCreator = artifacts.require('QuestionCreator');
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = (deployer) => {
+  deployer
+    .deploy([ERC20Token, ContractRegistry])
+    .then(() => deployer.deploy(QuestionCreator, ContractRegistry.address, ERC20Token.address))
+    .then(() => ContractRegistry.deployed())
+    .then(registry => registry.setRecorder(QuestionCreator.address));
 };

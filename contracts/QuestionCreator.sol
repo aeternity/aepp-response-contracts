e02ed4df3120378191cf513fa4baa7d98c7b31e6
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-12-01 19:05:46 +1000

    Use approveAndCall, update logic

--- contracts/QuestionCreator.sol
+++ contracts/QuestionCreator.sol
@@ -1,21 +1,56 @@
 pragma solidity ^0.4.15;
 
 import "./ContractRegistry.sol";
-import "./ERC20Token.sol";
+import "./AEToken/AEToken.sol";
 import "./Question.sol";
 
 contract QuestionCreator {
   ContractRegistry registry;
-  ERC20Interface token;
+  AEToken token;
+  address backend;
+  mapping(uint => address) charityAddresses;
 
-  function QuestionCreator(ContractRegistry _registry, ERC20Interface _token) {
+  function QuestionCreator(ContractRegistry _registry, AEToken _token, address _backend) {
     registry = _registry;
     token = _token;
+    backend = _backend;
+
+    charityAddresses[1] = 0xfA491DF8780761853D127A9f7b2772D688A0E3B5;
+    charityAddresses[2] = 0x45992982736870Fe45c41049C5F785d4E4cc38Ec;
+    charityAddresses[3] = 0xfA491DF8780761853D127A9f7b2772D688A0E3B5;
+    charityAddresses[4] = 0x45992982736870Fe45c41049C5F785d4E4cc38Ec;
   }
 
-  function create(string _question, string _twitterAccount, uint _deadline) returns (address) {
-    address newQuestion = new Question(token, _question, _twitterAccount, _deadline);
+  function receiveApproval(address from, uint256 value, address _tokenContract, bytes extraData) {
+    require(address(token) == _tokenContract);
+    require(address(token) == msg.sender);
+    require(value > 0);
+
+    uint padding = 32;
+
+    string memory twitterAccount;
+    assembly { twitterAccount := add(extraData, padding) }
+    padding += 32;
+    padding += bytes(twitterAccount).length + (32 - bytes(twitterAccount).length % 32) % 32;
+
+    string memory question;
+    assembly { question := add(extraData, padding) }
+    padding += 32;
+    padding += bytes(question).length + (32 - bytes(question).length % 32) % 32;
+
+    uint256 charityId;
+    assembly { charityId := mload(add(extraData, padding)) }
+    padding += 32;
+
+    uint256 deadline;
+    assembly { deadline := mload(add(extraData, padding)) }
+
+    require(charityAddresses[charityId] != 0);
+    Question newQuestion = new Question(
+      token, backend, twitterAccount, question,
+      charityAddresses[charityId], deadline,
+      from, value);
+    require(token.transferFrom(from, newQuestion, value));
     registry.add(newQuestion);
-    return newQuestion;
   }
 }
--- test/Question.js
+++ test/Question.js
@@ -1,123 +1,154 @@
 /* global artifacts contract it assert web3 */
 
+const Web3_1_0 = require('web3');
 const { assertError } = require('./common');
 
-const ERC20Token = artifacts.require('ERC20Token');
+const AEToken = artifacts.require('AEToken');
 const Question = artifacts.require('Question');
+const QuestionCreator = artifacts.require('QuestionCreator');
+const ContractRegistry = artifacts.require('ContractRegistry');
 
-const increaseTime = (seconds = 0) => web3.currentProvider.send({
-  jsonrpc: '2.0', method: 'evm_increaseTime', params: [seconds] }).result;
+const web3_1_0 = new Web3_1_0(new Web3_1_0.providers.HttpProvider('http://localhost:9000'));
+const encodeParameter = web3_1_0.eth.abi.encodeParameter.bind(web3_1_0.eth.abi);
+const encodeParameters = web3_1_0.eth.abi.encodeParameters.bind(web3_1_0.eth.abi);
+const encodeString = string => encodeParameter('string', string).slice(66);
+
+const week = 7 * 24 * 60 * 60;
+let badDeadline;
+let goodDeadline;
+
+const increaseTime = (seconds = 0) => new Promise((resolve, reject) =>
+  web3.currentProvider.send(
+    { jsonrpc: '2.0', method: 'evm_increaseTime', params: [seconds] },
+    (err, response) => {
+      if (err) reject(err);
+      else {
+        const increasedSeconds = response.result;
+        badDeadline = Math.floor(Date.now() / 1000) + (increasedSeconds + week - 300);
+        goodDeadline = Math.floor(Date.now() / 1000) + (increasedSeconds + week + 300);
+        resolve(increasedSeconds);
+      }
+    }));
 
 // https://github.com/ethereumjs/testrpc/issues/390
-const increasedSeconds = increaseTime();
-const testBeforeNowDate = Math.floor(Date.now() / 1000) + (increasedSeconds - 5);
-const testAfterNowDate = Math.floor(Date.now() / 1000) + (increasedSeconds + 5);
-const testQuestion = 'test_question';
-const testAccount = 'test_account';
-const testAnswer = 'test_answer';
+increaseTime().then(() => {
+  const testBackend = '0xfa491df8780761853d127a9f7b2772d688a0e3b5';
+  const testAccount = 'test_account';
+  const testQuestion = 'test_question';
+  const testAmount = 1;
+  const testCharityId = 2;
+  const testCharity = '0x45992982736870fe45c41049c5f785d4e4cc38ec';
+  const testAnswer = 'test_answer';
 
-contract('Question', (accounts) => {
-  it('can\'t be deployed with unset question', () =>
-    Question.new(ERC20Token.address, '', testAccount, testAfterNowDate)
-      .then(assert.fail, assertError));
+  contract('Question', (accounts) => {
+    it('should be deployable', () =>
+      Question.new(
+        AEToken.address, testBackend, testAccount, testQuestion,
+        testCharity, goodDeadline, accounts[0], testAmount)
+        .then(instance => instance.version.call().then(c => assert.equal(c, 1))));
 
-  it('can\'t be deployed with unset account', () =>
-    Question.new(ERC20Token.address, testQuestion, '', testAfterNowDate)
-      .then(assert.fail, assertError));
+    it('deadline must be at least one week after current date', () =>
+      Question.new(
+        AEToken.address, testBackend, testAccount, testQuestion,
+        testCharity, badDeadline, accounts[0], testAmount)
+        .then(assert.fail, assertError));
 
-  it('can\'t be deployed with deadline before current date', () =>
-    Question.new(ERC20Token.address, testQuestion, testAccount, testBeforeNowDate)
-      .then(assert.fail, assertError));
+    it('increase', () =>
+      Promise.all([
+        Question.new(
+          AEToken.address, testBackend, testAccount, testQuestion,
+          testCharity, goodDeadline, accounts[0], testAmount),
+        AEToken.deployed(),
+      ]).then(([question, token]) =>
+        token.approveAndCall(question.address, testAmount * 2,
+          encodeParameter('uint', 32 * 4), { from: accounts[1] })
+          .then(() => Promise.all([
+            question.donations().then(d => assert.equal(d, testAmount * 3)),
+            question.donorAmounts(accounts[0]).then(amount => assert.equal(amount, testAmount)),
+            question.donorAmounts(accounts[1]).then(amount => assert.equal(amount, testAmount * 2)),
+            question.highestDonors(0).then(([addr]) => assert.equal(addr, accounts[1])),
+            question.highestDonors(1).then(([addr]) => assert.equal(addr, accounts[0])),
+            question.donorCount().then(d => assert.equal(d, 2)),
+          ]))));
 
-  it('should be deployable', () =>
-    Question.new(ERC20Token.address, testQuestion, testAccount, testAfterNowDate)
-      .then(instance =>
-        Promise.all([
-          instance.version.call().then(c => assert.equal(c, 1)),
-          instance.getCharitiesCount.call().then(c => assert.equal(c, 4)),
-          instance.question.call().then(q => assert.equal(q, testQuestion)),
-          instance.twitterAccount.call().then(t => assert.equal(t, testAccount)),
-          instance.deadline.call().then(d => assert.equal(d, testAfterNowDate)),
-        ])));
+    it('can\'t increase after deadline', () =>
+      Promise.all([
+        Question.new(
+          AEToken.address, testBackend, testAccount, testQuestion,
+          testCharity, goodDeadline, accounts[0], testAmount),
+        AEToken.deployed(),
+      ]).then(([question, token]) =>
+        increaseTime(week + 1000)
+          .then(() => token.approveAndCall(question.address, testAmount * 2,
+            encodeParameter('uint', 32 * 4), { from: accounts[1] }))
+          .then(assert.fail, assertError)));
 
-  it('can\'t increase after deadline', () =>
-    Question.new(ERC20Token.address, testQuestion, testAccount, testBeforeNowDate)
-      .then(question => question.increase(0))
-      .then(assert.fail, assertError));
+    const createQuestion = () => {
+      const length =
+        encodeString(testAccount).length / 2 +
+        encodeString(testQuestion).length / 2 +
+        32 * 2;
+      const bytes = [
+        encodeParameters(['uint', 'uint'], [32 * 4, length]),
+        encodeString(testAccount),
+        encodeString(testQuestion),
+        encodeParameters(['uint', 'uint'], [testCharityId, goodDeadline]).slice(2),
+      ].join('');
 
-  const newDonatedQuestion = amount =>
-    Promise.all([
-      Question.new(ERC20Token.address, testQuestion, testAccount, testAfterNowDate),
-      ERC20Token.deployed(),
-    ])
-      .then(([question, token]) =>
-        token.approve(question.address, amount)
-          .then(() => question.increase(amount))
-          .then(() => question));
+      return Promise.all([
+        AEToken.deployed(),
+        QuestionCreator.deployed(),
+        ContractRegistry.deployed(),
+      ])
+        .then(([token, creator, registry]) =>
+          token.approveAndCall(creator.address, testAmount, bytes)
+            .then(() => registry.getContractsCount())
+            .then(count => registry.contracts(count - 1))
+            .then(contract => Question.at(contract))
+            .then(question => [question, token]));
+    };
 
-  it('increase', () => {
-    const amount = 1;
-    return newDonatedQuestion(amount)
-      .then(question =>
-        Promise.all([
-          question.donorDonations.call(accounts[0]).then(d => assert.equal(d, amount)),
-          question.donorCount.call().then(c => assert.equal(c, 1)),
-          question.donations.call().then(d => assert.equal(d, amount)),
-        ]));
-  });
+    it('answer', () =>
+      createQuestion()
+        .then(([question, token]) =>
+          token.balanceOf(testCharity)
+            .then(balanceBefore =>
+              question.answer(testAnswer, { from: accounts[1] })
+                .then(() => Promise.all([
+                  question.tweetUrl().then(t => assert.equal(t, testAnswer)),
+                  token.balanceOf(testCharity).then(balanceAfter =>
+                    assert.equal(balanceAfter, +balanceBefore + testAmount)),
+                ])))));
 
-  it('can\'t answer after deadline', () =>
-    Question.new(ERC20Token.address, testQuestion, testAccount, testBeforeNowDate)
-      .then(question => question.answer(testAnswer))
-      .then(assert.fail, assertError));
+    it('can\'t answer after deadline', () =>
+      createQuestion()
+        .then(([question]) =>
+          increaseTime(week + 1000)
+            .then(() => question.answer(testAnswer, { from: accounts[1] })))
+        .then(assert.fail, assertError));
 
-  it('answer', () =>
-    newDonatedQuestion(1)
-      .then(question =>
-        question.answer(testAnswer)
-          .then(() => question.tweetUrl.call()))
-      .then(t => assert.equal(t, testAnswer)));
+    it('answerable only by backend', () =>
+      createQuestion()
+        .then(([question]) => question.answer(testAnswer))
+        .then(assert.fail, assertError));
 
-  it('answer with transfer to charity', () => {
-    const amount = 1;
-    return Promise.all([
-      newDonatedQuestion(amount),
-      ERC20Token.deployed(),
-    ])
-      .then(([question, token]) =>
-        question.charities.call(0)
-          .then(([, charityAddress]) =>
-            token.balanceOf.call(charityAddress)
-              .then(charityBalance =>
-                question.answer(testAnswer)
-                  .then(() => token.balanceOf.call(charityAddress))
-                  .then(newCharityBalance =>
-                    assert.equal(newCharityBalance.comparedTo(charityBalance.plus(amount)), 0)))));
-  });
+    it('revert donation', () =>
+      createQuestion()
+        .then(([question, token]) =>
+          increaseTime(week + 1000)
+            .then(() => token.balanceOf(accounts[0]))
+            .then(balanceBefore =>
+              question.revertDonation()
+                .then(() => Promise.all([
+                  token.balanceOf(accounts[0])
+                    .then(balanceAfter => assert.equal(balanceAfter, +balanceBefore + testAmount)),
+                  question.donorRevertDonation(accounts[0])
+                    .then(revertDonation => assert.isTrue(revertDonation)),
+                ])))));
 
-  it('can\'t revert donation before deadline', () => {
-    const amount = 1;
-    return newDonatedQuestion(amount)
-      .then(question => question.revertDonation())
-      .then(assert.fail, assertError);
-  });
-
-  it('revert donation', () => {
-    const amount = 1;
-    return Promise.all([
-      newDonatedQuestion(amount)
-        .then((token) => {
-          increaseTime(10);
-          return token;
-        }),
-      ERC20Token.deployed(),
-    ])
-      .then(([question, token]) =>
-        token.balanceOf.call(accounts[0])
-          .then(accountBalance =>
-            question.revertDonation()
-              .then(() => token.balanceOf.call(accounts[0]))
-              .then(newAccountBalance =>
-                assert.equal(newAccountBalance.comparedTo(accountBalance.plus(amount)), 0))));
+    it('can\'t revert donation before deadline', () =>
+      createQuestion()
+        .then(([question, token]) => question.revertDonation())
+        .then(assert.fail, assertError));
   });
 });

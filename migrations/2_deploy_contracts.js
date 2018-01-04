dfb26286de3ee5e7fa3603722f59a5fb62e87c59
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-01-04 18:15:35 +1000

    Add kovan network
    http://truffleframework.com/tutorials/using-infura-custom-provider

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,18 +2,24 @@ const AEToken = artifacts.require('AEToken');
 const Response = artifacts.require('Response');
 
 module.exports = (deployer, environment, accounts) => {
-  deployer.deploy(AEToken, 1000000, 'test', 1, 'test')
+  deployer.deploy(AEToken, 1000000, 'test', 1, 'test', { overwrite: false })
     .then(() => AEToken.deployed())
     .then(token =>
       deployer.deploy(Response, token.address)
         .then(() => Response.deployed())
         .then(response => Promise.all([
-          response.setBackend(accounts[1]),
-          response.setFoundation(accounts[2], 1),
-          response.setFoundation(accounts[3], 1),
-          token.prefilled()
-            .then(prefilled =>
-              !prefilled && token.prefill(accounts, (new Array(10)).fill(10000))
-                .then(() => token.launch())),
+          ...environment === 'development' ? [
+            response.setBackend(accounts[1]),
+            response.setFoundation(accounts[2], 1),
+            response.setFoundation(accounts[3], 1),
+            token.prefilled()
+              .then(prefilled =>
+                !prefilled && token.prefill(accounts, (new Array(10)).fill(10000))
+                  .then(() => token.launch())),
+          ] : [],
+          ...environment === 'kovan' ? [
+            response.setBackend(accounts[0]),
+            response.setFoundation(accounts[0], 1),
+          ] : [],
         ])));
 };
--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +8,12 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 3500000
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/")
+      },
+      network_id: 42,
     }
   }
 };

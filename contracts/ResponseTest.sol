12c88501bc159d1993756ae24245b0725b551414
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-01-15 15:42:05 +1000

    Add support 3, 4, 5

--- contracts/ResponseTest.sol
+++ contracts/ResponseTest.sol
@@ -35,4 +35,62 @@ contract ResponseTest {
     supporters[msg.sender].amount += value;
     supporters[msg.sender].lastSupportAt = now;
   }
+
+
+  function support3(uint256 value) public {
+    suppAmount[msg.sender] += value;
+
+    uint sIdx;
+    for (uint i = 0; i <= 4; i++) {
+      if (highestSupp[i].account == msg.sender) return;
+      if (suppAmount[highestSupp[i].account] < suppAmount[highestSupp[sIdx].account]) sIdx = i;
+    }
+    if (suppAmount[highestSupp[sIdx].account] < suppAmount[msg.sender]) {
+      highestSupp[sIdx] = HighestSupporter({ account: msg.sender, lastSupportAt: now });
+    }
+  }
+
+
+  uint minIdx;
+
+  function updateMinIdx() internal {
+    uint sIdx;
+    for (uint i = 1; i <= 4; i++) {
+      if (suppAmount[highestSupp[i].account] < suppAmount[highestSupp[sIdx].account]) sIdx = i;
+    }
+    minIdx = sIdx;
+  }
+
+  function support4(uint256 value) public {
+    suppAmount[msg.sender] += value;
+
+    if (suppAmount[msg.sender] > suppAmount[highestSupp[minIdx].account]) {
+      for (uint i = 0; i <= 4; i++)
+        if (highestSupp[i].account == msg.sender) {
+          if (i == minIdx) updateMinIdx();
+          return;
+        }
+      highestSupp[minIdx] = HighestSupporter({ account: msg.sender, lastSupportAt: now });
+      updateMinIdx();
+    }
+  }
+
+  function support5(uint256 value) public {
+    suppAmount[msg.sender] += value;
+
+    if (suppAmount[msg.sender] <= suppAmount[highestSupp[minIdx].account]) return;
+    if (suppAmount[msg.sender] - value > suppAmount[highestSupp[minIdx].account]) return;
+
+    if (suppAmount[msg.sender] - value == suppAmount[highestSupp[minIdx].account]) {
+      if (highestSupp[minIdx].account == msg.sender) {
+        updateMinIdx();
+        return;
+      }
+      for (uint i = 0; i <= 4; i++)
+        if (highestSupp[i].account == msg.sender) return;
+    }
+
+    highestSupp[minIdx] = HighestSupporter({ account: msg.sender, lastSupportAt: now });
+    updateMinIdx();
+  }
 }

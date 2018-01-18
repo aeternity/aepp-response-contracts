cab82ce7da98ed0f2390406ea95abd265b8cad42
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-01-17 22:43:37 -0500

    quesiotns and comments

--- contracts/Response.sol
+++ contracts/Response.sol
@@ -82,20 +82,47 @@ contract Response {
   }
 
   function questionState(uint qIdx) internal view returns (QuestionState t) {
+    // packedState is all of the data about the question x`packed into a single integer
     uint packedState = s.qUint(qIdx, PACKED_STATE);
+
+    // the twitter user id is the last 64 bits of the packedState
     t.twitterUserId = uint64(packedState);
+
+    // packedstate is shifted right 64 bits
     packedState >>= 64;
+
+    // the tweet id is the last 64 bits
     t.tweetId = uint64(packedState);
+
+    // packedstate is shifted right 64 bits
     packedState >>= 64;
+
+    // the tweet creation date is the last 32 bits
     t.createdAt = uint32(packedState);
+
+    // packedstate is shifted right 32 bits
     packedState >>= 32;
+
+    // the supporter count is the last 32 bits
     t.supporterCount = uint32(packedState);
+
+    // packedstate is shifted right 32 bits
     packedState >>= 32;
+
+    // QUESTION: is my understanding correct?
+    // if remaining bits are all 0 then answered = false
+    // if any remaining bits are 1 then answered = true
+    // does this also need to have bool(packedState)? will that read only the last bit?
     t.answered = packedState & 1 != 0;
+    
+    // packedstate is shifted right 1 bit
     packedState >>= 1;
+
+    // highestLowestSupporterIdx is the last 8 bits
     t.highestLowestSupporterIdx = uint8(packedState);
   }
 
+  // writing to packedState opposite of previous function
   function setQuestionState(uint qIdx, QuestionState t) internal {
     uint packedState = t.highestLowestSupporterIdx;
     packedState <<= 1;
@@ -154,12 +181,18 @@ contract Response {
   function createQuestion(
     uint64 twitterUserId, bytes32 content, address foundation, uint amount
   ) payable external {
+    // read uint backend fee and make sure it is same as msg.value
     require(msg.value == s.rUint(BACKEND_FEE));
+    // transfer from sender to current address token amount
     require(token.transferFrom(msg.sender, this, amount));
+    // read boolean of foundation addressto check whether it is registered
     require(s.rBool(FOUNDATION, foundation));
     // todo twitter user id and content is unchecked
+    // QUESTION: what will it be checked for, same as foundation boolean?
 
+    // send backend address the backend fee
     s.rAddress(BACKEND).transfer(s.rUint(BACKEND_FEE));
+    // QUESTION: would gas be saved by using questionCount();
     uint qIdx = s.rUint(QUESTION_COUNT);
     s.setQ(qIdx, CONTENT, content);
     s.setQ(qIdx, AUTHOR, msg.sender);
@@ -177,6 +210,12 @@ contract Response {
     s.setR(QUESTION_COUNT, qIdx + 1);
   }
 
+// QUESTION: why do you want to keep track of highest supporter?
+// I had same question as last review. you're putting a for loop in a for loop in a non-view function
+// that could just as easily be performed off chain for no gas and no EVM lag
+// it doesn't seem that the highest supporter is needed as a value to be used in any solidity code,
+// only as a convenience for returning the value directly from solidity
+// instead of returning all bids and ordering them off chain.
   function updateHighestLowestSupporterIdx(uint qIdx) internal {
     uint8 sIdx;
     for (uint8 i = 1; i <= 4; i++) {
@@ -189,16 +228,18 @@ contract Response {
     qs.highestLowestSupporterIdx = sIdx;
     setQuestionState(qIdx, qs);
   }
-
+  // QUESTION: why does parameter address exist/not have a name?
   function receiveApproval(address from, uint256 value, address, bytes extraData) public {
+    // QUESTION: the sender of this transaction will be the AE token contract?
     require(address(token) == msg.sender);
     require(token.transferFrom(from, this, value));
+    // QUESTION: does this assign qIdx as the last 32 bits from the extraData variable?
     uint qIdx;
     assembly { qIdx := mload(add(extraData, 32)) }
     QuestionState memory qs = questionState(qIdx);
     require(now < qs.createdAt + 30 days);
     require(!qs.answered);
-
+    // increment amount value by the tokens contributed
     s.incQ(qIdx, AMOUNT, value);
     uint amountFrom = s.qUint(qIdx, SUPPORTER_AMOUNT, from);
     if (0 == amountFrom) {
@@ -209,6 +250,7 @@ contract Response {
     s.setQ(qIdx, SUPPORTER_AMOUNT, from, amountFrom);
 
     uint lowestIdx = qs.highestLowestSupporterIdx;
+    // if new amount is more than largest amount recalculate the top highest supporters
     if (s.qUint(qIdx, SUPPORTER_AMOUNT, s.qAddress(qIdx, HIGHEST_SUPPORTERS, lowestIdx, ACCOUNT))
       < amountFrom) {
       for (uint i = 0; i <= 4; i++)
